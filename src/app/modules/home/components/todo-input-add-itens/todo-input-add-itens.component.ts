81c522bd607729a24e452abfccdbabee01c2260c
Bruno Saturnino Alves <brunosaturnino55@gmail.com>
2023-05-17 22:52:11 -0300

    To-Do finalizado

--- src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-input-add-itens',
   templateUrl: './todo-input-add-itens.component.html',
   styleUrls: ['./todo-input-add-itens.component.scss']
 })
-export class TodoInputAddItensComponent {
+export class TodoInputAddItensComponent implements OnInit{
+
+  @Output() public emitItemTaskList = new EventEmitter();
+
+  public addItemTaskList: string = "";
+
+  construct() {}
+
+  ngOnInit(): void {}
+
+  public submitItemTaskList(): void {
+
+    this.addItemTaskList = this.addItemTaskList.trim();
+
+    if(this.addItemTaskList) {
+      this.emitItemTaskList.emit(this.addItemTaskList);
+      this.addItemTaskList = "";
+    }
+  }
 
 }
